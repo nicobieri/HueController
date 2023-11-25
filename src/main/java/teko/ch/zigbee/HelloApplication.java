5cf21539a4b4bb07f10ac0686f47c37465f8d495
nicobieri <nrbieri@outlook.com>
2023-11-25 16:10:12 +0100

    Ugly layout :)

--- src/main/java/teko/ch/zigbee/HelloApplication.java
+++ src/main/java/teko/ch/zigbee/HelloApplication.java
@@ -3,10 +3,10 @@ package teko.ch.zigbee;
 import com.fasterxml.jackson.databind.JsonNode;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import teko.ch.zigbee.HueGUI.HueGui;
 import teko.ch.zigbee.baseApi.ConfigManager;
 import teko.ch.zigbee.baseApi.HueBridgeController;
 import teko.ch.zigbee.baseApi.hueBridgeConnector;
-
 import javax.swing.*;
 import java.io.IOException;
 import java.util.HashMap;
@@ -73,11 +73,20 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        // JFrame frame = new JFrame("Hue Controller");
+        // HueContollerOverlay panel = new HueContollerOverlay();
+       // HueGui panel = new HueGui();
+       // frame.add(panel);
+       // frame.getContentPane().add(panel);
+       // frame.pack();
+       // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setVisible(true);
+
         JFrame frame = new JFrame("Hue Controller");
-        HueContollerOverlay panel = new HueContollerOverlay();
-        frame.add(panel);
-        frame.pack();
+        HueGui panel = new HueGui();
+        frame.setContentPane(panel); // Use this instead of frame.add(panel)
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack(); // This will size the frame based on the preferred sizes of its components
         frame.setVisible(true);
 
         launch();
